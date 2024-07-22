1f1fa04f7a8ad31fe17cc67364e503fb87587abf
youngseok <ergla88@gmail.com>
2024-07-22 23:10:24 +0900

    my first c# project for unit test

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public string Greet(string name)
+        {
+            return $"Hello, {name}!";
+        }
+    }
+}
