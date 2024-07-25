feef8d7e29de215a44f0957d706fd564898b0844
youngseok <ergla88@gmail.com>
2024-07-25 23:36:23 +0900

    study csharp chapter3 240725 variable

---
+++ chapter03/MyFirstApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    public class Program
+    {
+        enum RESPONSE_DATA {YES, NO, OK} // declare enum 
+
+        static void Main(string[] args)
+        {
+            int a = int.Parse("12345"); // string to int 
+            Console.WriteLine(a);
+
+            int c = 12345;
+            string d = c.ToString(); // int to string 
+            Console.WriteLine(d);
+            
+            var e = new int[] {10,20,30}; // var type and array
+            int? f = null; // nullable
+            
+            RESPONSE_DATA rd = RESPONSE_DATA.YES;
+            Console.WriteLine(rd == RESPONSE_DATA.NO);
+        }
+
+
+    }
+}
