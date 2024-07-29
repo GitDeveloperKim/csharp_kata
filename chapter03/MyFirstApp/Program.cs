0311a467f3f603f9f8c7cb2979a7b7033a0015d1
youngseok <ergla88@gmail.com>
2024-07-29 23:05:13 +0900

    chapter03 data form test

--- chapter03/MyFirstApp/Program.cs
+++ chapter03/MyFirstApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console; // console 사용 하는 문법인듯 
 
 namespace MyFirstApp
 {
@@ -20,6 +21,19 @@ namespace MyFirstApp
             
             RESPONSE_DATA rd = RESPONSE_DATA.YES;
             Console.WriteLine(rd == RESPONSE_DATA.NO);
+
+            // string format number 
+            // D
+            WriteLine("10진수: {0:D}", 123);
+            
+            // X
+            WriteLine("16진수: 0x{0:X}", 0xFF1234);
+
+            // N 
+            WriteLine("숫자 : {0:N}", 123456789);
+
+            // F
+            WriteLine("고정 소수점 : {0:F}", 123.45);
         }
 
 
