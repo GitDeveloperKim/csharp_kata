b3c111f331f68f0ae61599be5b31aad2700e7d40
youngseok <ergla88@gmail.com>
2024-07-29 23:06:21 +0900

    chapter03 add data format comment for study

--- chapter03/MyFirstApp/Program.cs
+++ chapter03/MyFirstApp/Program.cs
@@ -23,16 +23,16 @@ namespace MyFirstApp
             Console.WriteLine(rd == RESPONSE_DATA.NO);
 
             // string format number 
-            // D
+            // D : 10진수  
             WriteLine("10진수: {0:D}", 123);
             
-            // X
+            // X : 16진수 
             WriteLine("16진수: 0x{0:X}", 0xFF1234);
 
-            // N 
+            // N : 숫자
             WriteLine("숫자 : {0:N}", 123456789);
 
-            // F
+            // F : 고정 소수점 
             WriteLine("고정 소수점 : {0:F}", 123.45);
         }
 
