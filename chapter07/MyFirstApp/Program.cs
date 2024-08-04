f7cf0129e1cecc7257acdeee598e65927b23cad9
youngseok <ergla88@gmail.com>
2024-08-04 20:40:35 +0900

    add chapter07 this class example

--- chapter07/MyFirstApp/Program.cs
+++ chapter07/MyFirstApp/Program.cs
@@ -4,14 +4,40 @@ namespace MyFirstApp
 {
     public class Program
     {
+        class MyClass 
+        {
+            int a, b, c;
+
+            public MyClass()
+            {
+                this.a = 5425;
+                PrintFields();
+            }
+
+            public MyClass(int b) : this()
+            {
+                this.b = b;
+                PrintFields();
+            }
+
+            public MyClass(int b, int c) : this(b)
+            {
+                this.c = c;
+                PrintFields();
+            }
+
+            public void PrintFields()
+            {
+                Console.WriteLine($"a:{a}, b:{b}, c:{c}");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            MyClass a = new MyClass();
+            
+            MyClass b = new MyClass(1);
 
-        public string Greet(string name)
-        {
-            return $"Hello, {name}!";
+            MyClass c = new MyClass(10,20);
         }
     }
 }
