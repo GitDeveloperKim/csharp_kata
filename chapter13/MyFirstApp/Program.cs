0e4ea1c460c4ff18b63e629421013cef25a4d6e4
youngseok <ergla88@gmail.com>
2024-08-07 23:29:51 +0900

    add chapter13 delegate example

---
+++ chapter13/MyFirstApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    public class Program
+    {
+        delegate int MyDelegate(int a, int b);
+
+        class Calculator {
+            public int Plus(int a, int b)
+            {
+                return a+b;
+            }
+            public int Minus(int a, int b) 
+            {
+                return a-b;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Calculator Calc = new Calculator();
+            MyDelegate Callback;
+
+            Callback = new MyDelegate(Calc.Plus);
+            Console.WriteLine(Callback(3,4));
+
+            Callback = new MyDelegate(Calc.Minus);
+            Console.WriteLine(Callback(7,5));
+        }
+
+    }
+}
